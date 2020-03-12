a859ac1a786874c3fc4055121a81cd50bc432c71
David Joaquín Shourabi Porcel <david@djsp.eu>
2020-03-12 20:36:40 +0100

    FIXME

--- docman/setup.py
+++ docman/setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
         ],
     },
     install_requires=[
-        'boto3 == 1.12.14',
+        'boto3 == 1.12.19',
         'pyxdg == 0.26',
         'PyYAML == 5.3',
     ],
@@ -24,6 +24,7 @@ setuptools.setup(
     packages=packages,
     # f-strings were introduced in 3.6
     # Required argument was added to argparse.add_subparsers in 3.7
+    # importlib.resources was added in 3.7
     python_requires='>= 3.7',
     version='1.0',
 )
